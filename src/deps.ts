306f715ae3e4bd40df180c6842aa090fdcee4de4
Matt Johnston <m@ttjohnston.com>
2024-05-11 05:29:54 -0300

    no deps.ts file

--- src/deps.ts
+++
@@ -1,3 +0,0 @@
-export { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
-export { cyan } from "https://deno.land/std@0.192.0/fmt/colors.ts";
-export * as path from "https://deno.land/std@0.192.0/path/mod.ts";
